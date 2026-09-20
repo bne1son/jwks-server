c7b546de012a0ace5f127585d985a423e5aac122
bne1son <bennelsonta@gmail.com>
2026-09-20 13:27:46 -0500

    First gradebot run

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ use jose::{
     Jwk,
     Jwks,
     jwt_create_header,
-    jwt_create_payload
+    jwt_create_payload,
+    to_base64,
+    base64url_bytes
 };
 
 fn main() {
@@ -35,6 +37,9 @@ fn main() {
 // handles endpoints
 fn handle_connection(mut stream: TcpStream, key: &Key, expired_key: &Key) {
     let http_request = get_http_request(&stream);
+    let request_line: Vec<&str> = http_request[0].split_whitespace().collect();
+    let method = request_line[0];
+    let path = request_line[1];
     let now = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
@@ -56,17 +61,27 @@ fn handle_connection(mut stream: TcpStream, key: &Key, expired_key: &Key) {
         );
         stream.write_all(response.as_bytes()).unwrap();
     } else if http_request[0].starts_with("POST /auth") {
-                let header = jose::jwt_create_header(key);
-                let payload = jose::jwt_create_payload(key);
-                let encoded_header = jose::to_base64(&header);
-                let encoded_payload = jose::to_base64(&payload);
-                let header_payload = format!("{}.{}", encoded_header, encoded_payload);
-                let signature = key.sign(header_payload.as_bytes());
-                let encoded_signature = jose::base64url_bytes(&signature);
-                let jwt = format!("{}.{}.{}", encoded_header, encoded_payload, encoded_signature);
-                println!("JWT: {}", jwt);
-
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
+        let auth_key = if http_request[0].contains("?expired") {
+            expired_key
+        } else {
+            key
+        };
+        let header = jwt_create_header(auth_key);
+        let payload = jwt_create_payload(auth_key);
+        let encoded_header = to_base64(&header);
+        let encoded_payload = to_base64(&payload);
+        let header_and_payload = format!("{}.{}", encoded_header, encoded_payload);
+        let signature = auth_key.sign(header_and_payload.as_bytes());
+        let encoded_signature = base64url_bytes(&signature);
+        let jwt = format!("{}.{}.{}", encoded_header, encoded_payload, encoded_signature);
+        println!("JWT: {}", jwt);
+        let response = format!(
+            "HTTP/1.1 200 OK\r\n\
+            Content-Type: application/json\r\n\
+            \r\n\
+            {{\"token\":\"{}\"}}",
+            jwt
+        );
         stream.write_all(response.as_bytes()).unwrap();
     } else {
         println!("404");
